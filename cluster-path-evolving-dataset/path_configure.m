a2f586d10fa6873ce96f539b7281929e9653e760
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-06-25 16:50:09 -0400

    dd
    dd

--- cluster-path-evolving-dataset/path_configure.m
+++ cluster-path-evolving-dataset/path_configure.m
@@ -2,7 +2,8 @@ cd ~/Documents/cvx;
 cvx_setup;
 
 %add path
-cd ~/convex-clustering-evloving-dataset/cluster-path-evolving-dataset
+cd /localdisk/yawei1991/convex-clustering-evloving-dataset/cluster-path-evloving-dataset
 addpath(genpath('segment'));
+addpath(genpath('cpusmall'));
 addpath(genpath('svmguides-pendigits'));
 addpath(genpath('algo'));
