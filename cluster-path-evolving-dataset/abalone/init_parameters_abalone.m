70179600f119f8a0341f6e964fd896deeb69edb3
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-07-01 19:42:36 -0400

    d
    d

--- cluster-path-evolving-dataset/abalone/init_parameters_abalone.m
+++ cluster-path-evolving-dataset/abalone/init_parameters_abalone.m
@@ -23,6 +23,11 @@ s_hyp.TASK_TYPE = 'ridge_regression';
 %s_hyp.ALGO = 'CVX-PRIMAL-2011';
 s_hyp.ALGO = 'our_method';
 %s_hyp.ALGO = 'NETWORK_LASSO';
+if strcmp(s_hyp.ALGO, 'our_method')
+    s_hyp.beta = 1;
+else
+    s_hyp.beta = 0;
+end
 
 s_hyp.num_neighbours =5;
 s_hyp.alpha = 10; %hyper-parameter for regularized item, 10 in default
